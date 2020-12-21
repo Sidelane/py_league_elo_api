7558462817f0c849b2d9cbc6dacd74ed7cd8fafe
Sidelane <andreas.pelger@cit.fra-uas.de>
2020-12-21 11:08:12 +0100

    Rewrite to use Riot API

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from modules.summoner import Summoner
+from Summoner import Summoner
 from flask import Flask, jsonify
 
 app = Flask(__name__)
@@ -6,52 +6,69 @@ app = Flask(__name__)
 @app.route('/get_elo/<region>/<username>')
 def get_elo(region, username):
 
-    summoner = Summoner(username, region)
+    summoner = Summoner(region, username)
 
     if summoner.unranked == True:
-        return f"Summoner: {summoner.name} || Elo: Unranked"
+        return f"Summoner: {summoner.summoner_name} || Elo: Unranked"
+    elif summoner.promo == True:
+        return f"Summoner: {summoner.summoner_name} || Elo: {summoner.tier} {summoner.leaguePoints}LP Promo: {summoner.promoWins}-{summoner.promoLosses}"
     else:
-        return f"Summoner: {summoner.name} || Elo: {summoner.tier} {summoner.leaguepoints}"
+        return f"Summoner: {summoner.summoner_name} || Elo: {summoner.tier} {summoner.leaguePoints}LP"
 
 @app.route('/get_wr/<region>/<username>')
 def get_wr(region, username):
 
-    summoner = Summoner(username, region)
+    summoner = Summoner(region, username)
 
     if summoner.unranked == True:
-        return f"Summoner: {summoner.name} || Elo: Unranked"
+        return f"Summoner: {summoner.summoner_name} || Elo: Unranked"
     else:
-        return f"Summoner: {summoner.name} || {summoner.wins} {summoner.losses} - {summoner.winratio}"
+        return f"Summoner: {summoner.summoner_name} || {summoner.wins}W {summoner.losses}L - {summoner.winrate}%"
 
 @app.route('/get_full/<region>/<username>')
 def get_full(region, username):
 
-    summoner = Summoner(username, region)
+    summoner = Summoner(region, username)
 
     if summoner.unranked == True:
-        return f"Summoner: {summoner.name} || Elo: Unranked"
+        return f"Summoner: {summoner.summoner_name} || Elo: Unranked"
+    elif summoner.promo == True:
+        return f"Summoner: {summoner.summoner_name} || Elo: {summoner.tier} {summoner.leaguePoints}LP Promo: {summoner.promoWins}-{summoner.promoLosses} -- {summoner.wins}W {summoner.losses}L - {summoner.winrate}%"
     else:
-        return f"Summoner: {summoner.name} || Elo: {summoner.tier} {summoner.leaguepoints} -- {summoner.wins} {summoner.losses} - {summoner.winratio}"
+        return f"Summoner: {summoner.summoner_name} || Elo: {summoner.tier} {summoner.leaguePoints}LP -- {summoner.wins}W {summoner.losses}L - {summoner.winrate}%"
 
 @app.route('/json/get_full/<region>/<username>')
 def json_get_full(region, username):
     
-    summoner = Summoner(username, region)
+    summoner = Summoner(region, username)
 
     if summoner.unranked == True:
         return jsonify(
-            summoner = summoner.name,
+            summoner = summoner.summoner_name,
             unranked = summoner.unranked
+        )
+    elif summoner.promo == True:
+        return jsonify(
+            summoner = summoner.summoner_name,
+            region = summoner.region,
+            tier = summoner.tier,
+            leaguePoints = summoner.leaguePoints,
+            wins = summoner.wins,
+            losses = summoner.losses,
+            winratio = summoner.winrate,
+            miniSeries = summoner.promo,
+            miniSeriesWins = summoner.promoWins,
+            miniSeriesLosses = summoner.promoLosses
         )
     else:
         return jsonify(
-            summoner = summoner.name,
+            summoner = summoner.summoner_name,
             region = summoner.region,
             tier = summoner.tier,
-            lp = summoner.leaguepoints[:-3],
-            wins = summoner.wins[:-1],
-            losses = summoner.losses[:-1],
-            winratio = summoner.winratio[10:][:-1]
+            leaguePoints = summoner.leaguePoints,
+            wins = summoner.wins,
+            losses = summoner.losses,
+            winratio = summoner.winrate
         )
 
 if __name__ == '__main__':
